2a2ab51acc093ebeb3b5adcb57a27d541156c758
Mia Huang <102785000+MiaHG22@users.noreply.github.com>
2022-06-23 00:07:06 -0700

    app.js completed

--- js/app.js
+++ js/app.js
@@ -8,3 +8,14 @@ data.forEach((dataRow)=> {
     );
 });
 }
+function handleClick() {
+    let date = d3.select('#datetime').property('value');
+    let filteredData= tableData;
+    if (date) {
+        filteredData = filteredData.filter(row=>row.datetime === date);
+    }
+    buildTable(filteredData);
+}
+d3.selectAll('#filter-btn').on('click', handleClick);
+buildTable(tableData);
+
